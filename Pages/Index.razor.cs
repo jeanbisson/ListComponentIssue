16fc140af4b93b51d4ff0677357dbe88feb90f65
Jean Bisson <jean.bisson@hotmail.ca>
2022-05-19 20:07:08 -0400

    Add project files.

---
+++ Pages/Index.razor.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static ListComponentIssue.Shared.MyComponent;
+
+namespace ListComponentIssue.Pages
+{
+    public partial class Index
+    {
+        private List<FakeStuff> damn = new List<FakeStuff>();
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            damn.Add(
+                new FakeStuff()
+                {
+                    Id = "1",
+                    Title = "What"
+                }
+            );
+            damn.Add(
+                new FakeStuff()
+                {
+                    Id = "2",
+                    Title = "What"
+                }
+            );
+            damn.Add(
+             new FakeStuff()
+             {
+                 Id = "3",
+                 Title = "in"
+             }
+         );
+            damn.Add(
+             new FakeStuff()
+             {
+                 Id = "4",
+                 Title = "the"
+             }
+         );
+            damn.Add(
+             new FakeStuff()
+             {
+                 Id = "5",
+                 Title = "button"
+             }
+         );
+        }
+
+        public void close(FakeStuff what)
+        {
+            damn.Remove(what);
+        }
+
+    }
+}
